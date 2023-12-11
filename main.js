43d6a7301d2f87db6536e55bb1e9d36186172280
Mamatha Reddy Sampathi <102689196+smamatha1996@users.noreply.github.com>
2023-12-11 10:05:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeimg(){
+    document.getElementById("img1").src="https://wallpapers.com/images/featured/flower-pictures-unpxbv1q9kxyqr1d.jpg";
+}
+function changeimg1(){
+    document.getElementById("img2").src="https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+}
+function changeimg2(){
+    document.getElementById("img3").src="https://images.pexels.com/photos/46216/sunflower-flowers-bright-yellow-46216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+}
